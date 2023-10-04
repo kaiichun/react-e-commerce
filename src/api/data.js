79888714b6075470bf897115e38842ab96d08b86
Lee Kai Chun <kaiichun710@gmail.com>
2023-10-04 10:33:22 +0800

    update

--- src/api/data.js
+++ src/api/data.js
@@ -1,1 +1,6 @@
-export const API_URL = "http://localhost:8880";
+// export const API_URL = "http://localhost:8880";
+
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8880"
+    : "http://10.1.104.3";
